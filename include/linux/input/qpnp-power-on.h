c8bc2e91946d436c46cdbd85fd5e82f6f8a80584
dianlujitao <dianlujitao@lineageos.org>
2019-08-12 00:16:35 +0800

    power: reset: Import OnePlus changes
    Change-Id: I09433421fec3cd77f090d85e58c3b9f0daa8f324

--- include/linux/input/qpnp-power-on.h
+++ include/linux/input/qpnp-power-on.h
@@ -59,6 +59,8 @@ enum pon_restart_reason {
 	PON_RESTART_REASON_DMVERITY_CORRUPTED	= 0x04,
 	PON_RESTART_REASON_DMVERITY_ENFORCE	= 0x05,
 	PON_RESTART_REASON_KEYS_CLEAR		= 0x06,
+	/* add by yangrujin@bsp 2016/4/5, add op feature restart reason*/
+	PON_RESTART_REASON_REBOOT       = 0x10,
 
 	/* 32 ~ 63 for OEMs/ODMs secific features */
 	PON_RESTART_REASON_OEM_MIN		= 0x20,
