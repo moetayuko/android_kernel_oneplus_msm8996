b64f6942a836d531c1158a4553086919a073e2ba
dianlujitao <dianlujitao@lineageos.org>
2019-08-13 22:13:19 +0800

    power: Import OnePlus changes
    Change-Id: Id4514748bbee2ad946bb36a1598718de7c47eefc

--- drivers/power/supply/qcom/battery_current_limit.c
+++ drivers/power/supply/qcom/battery_current_limit.c
@@ -294,6 +294,20 @@ static int get_and_evaluate_battery_soc(void)
 	union power_supply_propval ret = {0,};
 	int battery_percentage;
 	enum bcl_threshold_state prev_soc_state;
+/* taokai@bsp add for detecting usb status 2016.03.11 */
+	static struct power_supply *usb_psy;
+	int usb_state;
+	bool is_usb_present;
+
+/* taokai@bsp add for detecting usb status 2016.03.11 */
+	if (!usb_psy)
+		usb_psy = power_supply_get_by_name("usb");
+	if (usb_psy) {
+		usb_state = usb_psy->get_property(usb_psy,
+				POWER_SUPPLY_PROP_PRESENT, &ret);
+		if (usb_state == 0)
+			is_usb_present = ret.intval;
+	}
 
 	if (!batt_psy)
 		batt_psy = power_supply_get_by_name("battery");
@@ -305,7 +319,12 @@ static int get_and_evaluate_battery_soc(void)
 		pr_debug("Battery SOC reported:%d", battery_soc_val);
 		trace_bcl_sw_mitigation("SoC reported", battery_soc_val);
 		prev_soc_state = bcl_soc_state;
-		bcl_soc_state = (battery_soc_val <= soc_low_threshold) ?
+/* taokai@bsp add for detecting usb status 2016.03.11 */
+		pr_debug("is_usb_present:%d", is_usb_present);
+		if(is_usb_present)
+			bcl_soc_state = BCL_HIGH_THRESHOLD;
+		else
+			bcl_soc_state = (battery_soc_val <= soc_low_threshold) ?
 					BCL_LOW_THRESHOLD : BCL_HIGH_THRESHOLD;
 		if (bcl_soc_state == prev_soc_state)
 			return NOTIFY_OK;
